4d210ec3cc8e5e64cf4ce0a960104e58046d7e6a
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2020-09-20 22:40:52 +0300

    feat: make message component HW1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,33 +1,26 @@
-import React from "react";
+import React from 'react';
+import Message from './Message';
+// import img from './img/avatar.jpg';
 
 const messageData = {
-    avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
-    time: "22:00",
+  avatarImg: './img/avatar.jpg',
+  name: 'Yaroslav MIg',
+  message:
+    'React разработан вокруг концепции многоразовых компонентов. Вы определяете небольшие компоненты, и объединяете их, чтобы сформировать более крупные компоненты.',
+  time: '21:00',
 };
 
 function HW1() {
-    return (
-        <div>
-            <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
-        </div>
-    );
+  return (
+    <>
+      <Message
+        avatar={messageData.avatarImg}
+        name={messageData.name}
+        message={messageData.message}
+        time={messageData.time}
+      />
+    </>
+  );
 }
 
 export default HW1;
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React from 'react';
+import style from './Message.module.css';
 
-function Message() {
-    return (
-        <div>
+type PropsType = {
+  avatar: string;
+  name: string;
+  message: string;
+  time: string;
+};
 
-        </div>
-    );
+// require(`${props.avatar}`)
+export function Message(props: PropsType) {
+  return (
+    <article className={style.container}>
+      <div className={style.box_img}>
+        <img
+          className={style.avatar}
+          src={require(`${props.avatar}`)}
+          alt='avatar'
+          title={props.name}
+        />
+      </div>
+      <div className={style.box_message}>
+				<h4 className={ style.name }>{props.name}</h4>
+        <p className={style.text}>
+          {props.message}
+          <time className={style.time} dateTime='20:00'>{props.time}</time>
+        </p>
+      </div>
+    </article>
+  );
 }
 
 export default Message;
